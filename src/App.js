5853e2630915ac9987157db4084e7481f99a9ca1
Antony Caseres <accasere@outlook.com>
2022-07-25 17:23:59 -0500

    Add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header'
+import Products from './components/Products'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
+import Cart from './components/Cart'
+import AddProduct from './components/AddProduct'
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header/>}>
+            <Route path="/products" component={Products} />
+            <Route path="/products" component={SignUp} />
+            <Route path="/products" component={Login} />
+            <Route path="/products" component={Cart} />
+            <Route path="/products" component={AddProduct} />
+
+
+
+          </Route>
+        </Routes>
+      
+      
+      </BrowserRouter>
+     
   );
 }
 
